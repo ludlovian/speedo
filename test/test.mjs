a3e6ba1de1482584866d01176c8306144cde3a24
Alan Layng <alan@readersludlow.com>
2021-04-24 22:38:24 +0100

    s/ava/uvu/

--- test/test.mjs
+++ test/test.mjs
@@ -1,36 +1,39 @@
-import test from 'ava'
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
 
 import Speedo from '../src/index.mjs'
 
-test('basic run', async t => {
+test('basic run', async () => {
   const sp = new Speedo()
-  t.is(sp.rate(), 0)
-  t.is(sp.eta(), 0)
-  t.is(sp.percent(), null)
+  assert.is(sp.rate(), 0)
+  assert.is(sp.eta(), 0)
+  assert.is(sp.percent(), null)
 
   await delay(100)
   sp.update({ current: 1, total: 5 })
 
-  t.false(sp.done)
-  t.true(roughly(sp.taken(), 100))
-  t.true(roughly(sp.percent(), 20))
-  t.true(roughly(sp.eta(), 400))
-  t.true(roughly(sp.rate(), 10))
+  assert.not.ok(sp.done)
+  roughly(sp.taken(), 100)
+  roughly(sp.percent(), 20)
+  roughly(sp.eta(), 400)
+  roughly(sp.rate(), 10)
 
   await delay(400)
   sp.update(5)
 
-  t.true(sp.done)
-  t.true(roughly(sp.taken(), 500))
-  t.is(sp.eta(), 0)
-  t.true(roughly(sp.rate(), 10))
-  t.is(sp.percent(), 100)
+  assert.ok(sp.done)
+  roughly(sp.taken(), 500)
+  assert.is(sp.eta(), 0)
+  roughly(sp.rate(), 10)
+  assert.is(sp.percent(), 100)
 })
+
+test.run()
 
 function roughly (a, b, tolerance = 0.05) {
   const diff = Math.abs(b - a)
   const larger = Math.max(a, b)
-  return diff / larger < tolerance
+  assert.ok(diff / larger < tolerance)
 }
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
